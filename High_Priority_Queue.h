2e916b733ab04f91c607f342d5b5bb4e963cc6e3
AbdulShabazz <AbdulShabazz@users.noreply.github.com>
2023-05-26 04:31:39 -0400

    Create High_Priority_Queue.h

---
+++ High_Priority_Queue.h
@@ -0,0 +1,119 @@
+#include <vector>
+
+template<typename T, std::size_t N>
+class SIMD_Stack {
+public:
+    SIMD_Stack() {
+        for(auto& vec : vectors) {
+            vec.push_back(1);
+        }
+    }
+    
+    std::vector<T>& operator[](std::size_t i) {
+        return vectors[i];
+    }
+    
+    const std::vector<T>& operator[](std::size_t i) const {
+        return vectors[i];
+    }
+    
+private:
+    std::vector<std::vector<T>, N> vectors;
+};
+
+/*
+/ *
+Yes, it is possible to use copy-on-write to allow a std::priority_queue::push and std::priority_queue::top operation to achieve O(1) runtime complexity.
+
+Copy-on-write is a technique that allows multiple processes to share the same memory without having to make a copy of it. This is done by creating a reference count for each piece of memory. When a process wants to modify a piece of memory, it increments the reference count. If another process also wants to modify the same piece of memory, it also increments the reference count. If the reference count reaches 2, then a copy of the memory is made and the two processes can now modify their own copies without affecting each other.
+
+In the case of a std::priority_queue, we can use copy-on-write to allow multiple threads to push and pop elements without having to lock the queue. When a thread wants to push an element, it first checks to see if the reference count for the queue is 1. If it is, then it simply increments the reference count and pushes the element onto the queue. If the reference count is greater than 1, then the thread creates a copy of the queue and pushes the element onto the copy. The original queue is then left untouched.
+
+This technique allows us to achieve O(1) runtime complexity for both the push and pop operations. The push operation only has to increment the reference count, which is a constant-time operation. The pop operation only has to check the reference count and then return the top element. If the reference count is 1, then the top element is simply returned. If the reference count is greater than 1, then the top element is copied from the original queue and returned.
+
+Here is an example of how to implement copy-on-write for a std::priority_queue:
+C++
+
+#include <queue>
+
+class CopyOnWritePriorityQueue {
+public:
+  CopyOnWritePriorityQueue() {}
+
+  void push(int value) {
+    if (reference_count == 1) {
+      queue_.push(value);
+    } else {
+      queue_ = queue_.copy();
+      queue_.push(value);
+    }
+    ++reference_count;
+  }
+
+  int top() {
+    if (reference_count == 1) {
+      return queue_.top();
+    } else {
+      return queue_.copy().top();
+    }
+  }
+
+  void pop() {
+    if (reference_count == 1) {
+      queue_.pop();
+    } else {
+      queue_ = queue_.copy();
+      queue_.pop();
+    }
+    --reference_count;
+  }
+
+private:
+  std::queue<int> queue_;
+  int reference_count = 1;
+};
+
+Use code with caution. Learn more
+
+This implementation achieves O(1) runtime complexity for both the push and pop operations. The push operation only has to increment the reference count, which is a constant-time operation. The pop operation only has to check the reference count and then return the top element. If the reference count is 1, then the top element is simply returned. If the reference count is greater than 1, then the top element is copied from the original queue and returned.
+* /
+#include <queue>
+
+class CopyOnWritePriorityQueue {
+public:
+  CopyOnWritePriorityQueue() {}
+
+  void push(int value) {
+    if (reference_count == 1) {
+      queue_.push(value);
+    } else {
+      queue_ = queue_.copy();
+      queue_.push(value);
+    }
+    ++reference_count;
+  }
+
+  int top() {
+    if (reference_count == 1) {
+      return queue_.top();
+    } else {
+      return queue_.copy().top();
+    }
+  }
+
+  void pop() {
+    if (reference_count == 1) {
+      queue_.pop();
+    } else {
+      queue_ = queue_.copy();
+      queue_.pop();
+    }
+    --reference_count;
+  }
+
+private:
+  std::queue<int> queue_;
+  int reference_count = 1;
+};
+
+*/
